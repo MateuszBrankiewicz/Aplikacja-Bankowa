e06fc3247b6af68f3bd7832a4d6af3b98bb9e713
Squashim <konrad18v1@wp.pl>
2023-04-19 19:33:36 +0200

    Added pin page layout

--- lib/components/logo.dart
+++ lib/components/logo.dart
@@ -1,26 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Logo extends StatelessWidget {
-  final String imagePath;
-  const Logo({
-    super.key,
-    required this.imagePath,
-  });
+class VectorBackgroundLogo extends StatelessWidget {
+  final double fem;
+  final Widget child;
+
+  const VectorBackgroundLogo(
+      {super.key, required this.fem, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
-      ),
-      child: Image.asset(
-        imagePath,
-        height: 50,
-        width: 60,
-      ),
+    return Stack(
+      children: [
+        // Vector BG
+        Positioned(
+          left: 0 * fem,
+          top: 0 * fem,
+          child: Align(
+            child: FittedBox(
+              child: Image.asset(
+                './lib/images/vector_bg.png',
+                width: 630.91 * fem,
+                height: 356 * fem,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 100 * fem,
+          top: 46 * fem,
+          child: SizedBox(
+            width: 159 * fem,
+            height: 108 * fem,
+            child: SizedBox(
+              width: double.infinity,
+              height: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    // logo
+                    left: 36 * fem,
+                    top: 0 * fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 89 * fem,
+                        height: 89 * fem,
+                        child: Image.asset(
+                          './lib/images/logo.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0 * fem,
+                    top: 64 * fem,
+                    child: Align(
+                      child: SizedBox(
+                        width: 300 * fem,
+                        height: 44 * fem,
+                        child: Text(
+                          'Polonia Bank',
+                          style: GoogleFonts.glegoo(
+                            fontSize: 24 * fem,
+                            fontWeight: FontWeight.w700,
+                            height: 1.7925,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(child: child),
+      ],
     );
   }
 }
