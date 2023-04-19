08cb65464e6add3b5e77032af826287d802447d3
xMatty522 <bronczek522@gmail.com>
2023-04-19 16:36:42 +0200

    register/login 100% done

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:appbank/pages/pin_page.dart';
 import 'package:flutter/material.dart';
 import 'package:appbank/components/my_button.dart';
 import 'package:appbank/components/my_textfield.dart';
@@ -103,8 +106,8 @@ class _SignupPageState extends State<SignupPage> {
           firstName, lastName, email, password, confirmPassword);
 
       if (isRegistered) {
-        // Registration successful
-        // Navigate to home page or show a success message
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => PinInputScreen()));
       } else {
         // Registration failed
         // Show an error message
@@ -535,119 +538,4 @@ class _SignupPageState extends State<SignupPage> {
               ),
             ])));
   }
-//     return Scaffold(
-//       backgroundColor: Colors.white,
-//       body: SafeArea(
-//         child: Center(
-//           child: SingleChildScrollView(
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
-//               children: [
-//                 const SizedBox(height: 45),
-// // logo
-//                 const Logo(imagePath: './lib/images/logo.jfif'),
-//                 const SizedBox(height: 25),
-
-//                 const Text(
-//                   'Bank Polonia',
-//                   style: TextStyle(
-//                     color: Colors.red,
-//                     fontSize: 35,
-//                   ),
-//                 ),
-
-//                 const SizedBox(height: 15),
-
-//                 // registration form
-//                 Padding(
-//                   padding: const EdgeInsets.symmetric(horizontal: 20.0),
-//                   child: Column(
-//                     children: [
-//                       Row(
-//                         children: [
-//                           Expanded(
-//                             child: MyTextField(
-//                               controller: _firstNameController,
-//                               hintText: 'First Name',
-//                               errorText: _firstNameError,
-//                               obscureText: false,
-//                             ),
-//                           ),
-//                           const SizedBox(width: 10),
-//                           Expanded(
-//                             child: MyTextField(
-//                               controller: _lastNameController,
-//                               hintText: 'Last Name',
-//                               errorText: _lastNameError,
-//                               obscureText: false,
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                       const SizedBox(height: 10),
-//                       MyTextField(
-//                         hintText: 'Email',
-//                         controller: _emailController,
-//                         errorText: _emailError,
-//                         obscureText: false,
-//                       ),
-//                       const SizedBox(height: 10),
-//                       MyTextField(
-//                         hintText: 'Password',
-//                         controller: _passwordController,
-//                         errorText: _passwordError,
-//                         obscureText: true,
-//                       ),
-//                       const SizedBox(height: 10),
-//                       MyTextField(
-//                         hintText: 'Confirm Password',
-//                         controller: _confirmPasswordController,
-//                         errorText: _confirmPasswordError,
-//                         obscureText: true,
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-
-//                 const SizedBox(height: 20),
-
-//                 // sign up button
-//                 MyButton(
-//                   buttonText: 'Sign Up',
-//                   onTap: () => signUp(),
-//                 ),
-
-//                 const SizedBox(height: 20),
-
-//                 // already a member? sign in now
-//                 Row(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: [
-//                     Text(
-//                       'Already a member?',
-//                       style: TextStyle(color: Colors.grey[700]),
-//                     ),
-//                     const SizedBox(width: 4),
-//                     TextButton(
-//                       onPressed: () {
-//                         Navigator.pop(context);
-//                       },
-//                       child: const Text(
-//                         'Sign in now',
-//                         style: TextStyle(
-//                           color: Colors.blue,
-//                           fontWeight: FontWeight.bold,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 }
