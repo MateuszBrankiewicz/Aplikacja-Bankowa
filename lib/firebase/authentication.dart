a2bab072207c3557eedcb88d160b3423090afe84
xMatty522 <bronczek522@gmail.com>
2023-04-19 13:27:39 +0200

    signup page do naprawy nie dziala przycisk i tekst

--- lib/firebase/authentication.dart
+++ lib/firebase/authentication.dart
@@ -1,15 +1,23 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:appbank/pages/pin_page.dart';
+import 'package:flutter/material.dart';
 
 class Authentication {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   Future<String?> signInWithEmailAndPassword(
-      String email, String password) async {
+      BuildContext context, String email, String password) async {
     try {
       final UserCredential userCredential =
           await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PinInputScreen()),
       );
       final User? user = userCredential.user;
       return user?.uid;
