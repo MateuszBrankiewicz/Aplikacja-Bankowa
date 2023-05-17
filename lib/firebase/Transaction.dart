748563f0d52d55cfc2a01c56432b0a3817938ba2
xMatty522 <bronczek522@gmail.com>
2023-05-17 22:11:38 +0200

    transaction

--- lib/firebase/Transaction.dart
+++ lib/firebase/Transaction.dart
@@ -3,116 +3,65 @@ import 'package:appbank/pages/payments_screens.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 void wykonajPrzelew(Transfer transfer) async {
-  // try {
-  //   final recipient = await sprawdzNumerKonta(transfer.accNumber);
-  //   if (recipient == null) {
-  //     throw Exception('Niepoprawny numer konta');
-  //   }
+  try {
+    User? user = FirebaseAuth.instance.currentUser;
+    String userId = user!.uid;
+    DateTime data = DateTime.now();
+    final senderData =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
 
-  //   User? user = FirebaseAuth.instance.currentUser;
-  //   String userId = user!.uid;
-  //   final db = FirebaseFirestore.instance;
+    final receivingData = await FirebaseFirestore.instance
+        .collection('users')
+        .where('Bank account number', isEqualTo: transfer.accNumber)
+        .get();
 
-  //   // Use a transaction to ensure atomic updates
-  //   final result = await db.runTransaction((transaction) async {
-  //     // Check if the sender has sufficient funds
-  //     final senderDoc =
-  //         await transaction.get(db.collection('users').doc(userId));
-  //     final senderData = senderDoc.data();
-  //     if (senderData == null || !senderData.containsKey('account balance')) {
-  //       throw Exception('Nie znaleziono pola "account balance"');
-  //     }
-  //     final senderBalance = double.parse(senderData['account balance']);
+    if (receivingData.docs.isEmpty) {
+      throw Exception('Niepoprawny numer konta');
+    }
 
-  //     if (senderBalance < transfer.amount) {
-  //       throw Exception('Niewystarczające środki na koncie');
-  //     }
+    final receivingDoc = receivingData.docs.first;
+    final senderBalance = double.parse(senderData['account balance']);
 
-  //     // Update the recipient's account balance
-  //     final recipientRef = db.collection('users').doc(recipient['userId']);
-  //     final recipientBalance = double.parse(recipient['account balance']);
-  //     final newRecipientBalance = recipientBalance + transfer.amount;
-  //     transaction.update(
-  //         recipientRef, {'account balance': newRecipientBalance.toString()});
+    if (senderBalance < transfer.amount) {
+      throw Exception('Niewystarczające środki na koncie');
+    }
 
-  //     // Update the sender's account balance
-  //     final senderRef = db.collection('users').doc(transfer.sender);
-  //     final newSenderBalance = senderBalance - transfer.amount;
-  //     transaction
-  //         .update(senderRef, {'account balance': newSenderBalance.toString()});
+    final newSenderBalance = senderBalance - transfer.amount;
+    final newReceivingBalance =
+        double.parse(receivingDoc['account balance']) + transfer.amount;
 
-  //     // Add a new transaction to the transaction history
-  //     final transactionsRef = db.collection('transactions').doc();
+    // Use a batched write to update multiple documents atomically
+    WriteBatch batch = FirebaseFirestore.instance.batch();
 
-  //     transaction.set(transactionsRef, transfer.toJson());
-  //   });
+    // Update sender's account balance
+    batch.update(FirebaseFirestore.instance.collection('users').doc(userId),
+        {'account balance': newSenderBalance.toString()});
 
-  //   if (result == null) {
-  //     throw Exception('Błąd podczas aktualizacji kont');
-  //   }
-  // } catch (e) {
-  //   throw Exception('Wystąpił błąd: $e');
-  // }
-  User? user = FirebaseAuth.instance.currentUser;
-  String userId = user!.uid;
-  final senderData =
-      await FirebaseFirestore.instance.collection('users').doc(userId).get();
-  print(transfer.accNumber);
-  final receivingData = await FirebaseFirestore.instance
-      .collection('users')
-      .where('Bank account number', isEqualTo: transfer.accNumber)
-      .get(); // Call `get()` to execute the query and retrieve the `QuerySnapshot`.
+    // Update recipient's account balance
+    batch.update(
+        FirebaseFirestore.instance.collection('users').doc(receivingDoc.id),
+        {'account balance': newReceivingBalance.toString()});
 
-  // if (receivingData.docs.isEmpty) {
-  //   // Throw an error if the `QuerySnapshot` is empty (i.e., no documents were found).
-  //   throw Exception('Niepoprawny numer konta');
-  // }
+    // Add a new transaction to the sender's transaction history
+    String transactionEntry =
+        "accNumber: ${transfer.accNumber.toString()}, firstName: ${senderData['First Name']}, lastName: ${senderData['Last Name']}, amount: ${transfer.amount.toString()}, wheter: false, title: ${transfer.title}, data: $data";
 
-  final receivingDoc =
-      receivingData.docs.first.data(); // Get the data for the first document.
+    batch.update(FirebaseFirestore.instance.collection('users').doc(userId), {
+      'transaction': FieldValue.arrayUnion([transactionEntry])
+    });
 
-  senderData.data();
-  String senderNewBalance =
-      (double.parse(senderData['account balance']) - transfer.amount)
-          .toString();
+    // Add a new transaction to the recipient's transaction history
+    transactionEntry =
+        "accNumber: ${transfer.accNumber.toString()}, firstName: ${senderData['First Name']}, lastName: ${senderData['Last Name']}, amount: ${transfer.amount.toString()}, wheter: false, title: ${transfer.title}, data: $data";
 
-  String receivingBalance =
-      (double.parse(receivingDoc['account balance']) + transfer.amount)
-          .toString();
+    batch.update(
+        FirebaseFirestore.instance.collection('users').doc(receivingDoc.id), {
+      'transaction': FieldValue.arrayUnion([transactionEntry])
+    });
 
-  await FirebaseFirestore.instance
-      .collection('users')
-      .doc(userId)
-      .update({'account balance': senderNewBalance});
-  await FirebaseFirestore.instance
-      .collection('users')
-      .doc(receivingData.docs.first.id)
-      .update({'account balance': receivingBalance});
-  transfer.sender = senderData['First Name'] + ' ' + senderData['Last Name'];
-  await FirebaseFirestore.instance
-      .collection('users')
-      .doc(userId)
-      .update({'transaction': transfer.toJson()});
-  transfer.whether = false;
-  await FirebaseFirestore.instance
-      .collection('users')
-      .doc(receivingData.docs.first.id)
-      .update({'transaction': transfer.toJson()});
+    // Commit the batched write
+    await batch.commit();
+  } catch (e) {
+    throw Exception('Wystąpił błąd: $e');
+  }
 }
-
-// Future<Map<String, dynamic>?> sprawdzNumerKonta(String numerKonta) async {
-//   final db = FirebaseFirestore.instance;
-//   try {
-//     final snapshot = await db
-//         .collection('users')
-//         .where('Bank account number', isEqualTo: numerKonta)
-//         .get();
-//     print(snapshot);
-//     if (snapshot.docs.isNotEmpty) {
-//       return snapshot.docs.first.data() as Map<String, dynamic>;
-//     }
-//   } catch (e) {
-//     throw Exception('Wystąpił błąd: $e');
-//   }
-//   return null;
-// }
